48fc3bf8d205aadf66b5d9f7bb00225902995f25
Roromo0306 <pownyrainbow03@gmail.com>
2025-10-29 17:28:57 +0100

    bug arreglado

--- SpookyHotel/Assets/Scripts/Clientes/ClienteManager.cs
+++ SpookyHotel/Assets/Scripts/Clientes/ClienteManager.cs
@@ -16,7 +16,7 @@ public class ClienteManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvocarCliente();
     }
 
     // Update is called once per frame
